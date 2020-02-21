ddb75698e08d276761ab35472b712ce8b3a6af75
Ameya Daigavane <ameya.d.98@gmail.com>
2020-02-22 03:50:08 +0530

    Updates so that build/run works correctly.

--- src/main.rs
+++ src/main.rs
@@ -8,25 +8,26 @@ mod transport;
 mod random;
 
 use config::{
-    CCConfig, Config, ConfigLog, ConfigTopo, LinkTraceConfig, LogType, SenderGroupConfig,
+    CCConfig, Config, ConfigLog, ConfigTopo, LinkTraceConfig, LogType, SenderGroupConfig, DelayConfig,
+};
+use random::{
+    RandomVariable, seed,
 };
 use simulator::*;
 use topology::create_topology;
 use tracer::Tracer;
 use transport::*;
-use random::RandomVariable;
 
 use rand_distr::Poisson;
-use rand::SeedableRng;
 
 use failure::Error;
 
 fn main() -> Result<(), Error> {
-    // Three variants of links to choose from
+    // Four variants of links to choose from
     let _c_link_trace = LinkTraceConfig::Const(15_000_000.);
     let _r_link_trace = LinkTraceConfig::Random(RandomVariable{
-        dist: Poisson::new(1_000_000.0).unwrap(),
-        rng: SeedableRng::from_seed([0; 32]),
+        dist: Poisson::new(1_000_000.).unwrap(),
+        offset: 14_000_000.,
     });
     let _p_link_trace = LinkTraceConfig::Piecewise(vec![
         (1_500_000., Time::from_secs(20)),
@@ -39,7 +40,7 @@ fn main() -> Result<(), Error> {
     for i in 0..2 {
         sender_groups.push(SenderGroupConfig {
             num_senders: 1,
-            delay: Time::from_millis(50),
+            delay: DelayConfig::Const(Time::from_millis(50)),
             cc: CCConfig::Instant,
             start_time: Time::from_secs(i * 2),
             tx_length: TcpSenderTxLength::Infinite,
@@ -65,8 +66,10 @@ fn main() -> Result<(), Error> {
             link_rates: LogType::Plot,
             link_bucket_size: Time::from_micros(1_000_000),
         },
+        random_seed: 0,
     };
 
+    seed(config.random_seed);
     let tracer = Tracer::new(&config);
     let mut sched = create_topology(&config, &tracer)?;
 
