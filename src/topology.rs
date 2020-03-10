3bf1506f1426baf5b94bb6a791023c87f0cf7a4b
Venkat Arun <venkatar@mit.edu>
2020-03-10 00:39:49 -0400

    Fix topology to have different ackers for different senders

--- src/topology.rs
+++ src/topology.rs
@@ -8,26 +8,22 @@ use crate::transport::*;
 use failure::Error;
 
 /// Creates topology specified in Config and returns a Scheduler (with appropriate NetObjects). The
-/// base topology is as follows (tcp_sender -> delay) -> link -> acker -> router --...--> back to
-/// senders
+/// base topology is as follows (tcp_sender -> delay) -> link -> router --..--> ackers --> back to
+/// corresponding senders
 pub fn create_topology<'a>(config: &'a Config, tracer: &'a Tracer) -> Result<Scheduler<'a>, Error> {
-    //let tracer = Tracer::new(&config);
     let mut sched = Scheduler::default();
 
     let link_id = sched.next_obj_id();
-    let acker_id = link_id + 1;
-    let router_id = acker_id + 1;
+    let router_id = link_id + 1;
 
     // Create bottleneck
     let link_trace = LinkTrace::from_config(&config.topo.link, config)?;
-    let link = Link::new(link_trace, config.topo.bufsize, acker_id, &tracer, &config);
-    let acker_addr = sched.next_addr();
-    let acker = Acker::new(acker_addr, router_id);
+    let link = Link::new(link_trace, config.topo.bufsize, router_id, &tracer, &config);
     let mut router = Router::new(sched.next_addr());
 
     // Register the core objects. Remember to do it in the same order as the ids
     sched.register_obj(Box::new(link));
-    sched.register_obj(Box::new(acker));
+    //sched.register_obj(Box::new(acker));
 
     // List of objects we need to register, in the order we should register them. Before
     // registering these, we'll register router
@@ -45,8 +41,11 @@ pub fn create_topology<'a>(config: &'a Config, tracer: &'a Tracer) -> Result<Sch
             // Decide everybody's ids
             let tcp_sender_id = router_id + 1 + objs_to_reg.len();
             let delay_id = tcp_sender_id + 1;
+            let acker_id = delay_id + 1;
 
-            // Create the sender
+            let acker_addr = sched.next_addr();
+
+            // Create the sender and its delay module
             let sender_addr = sched.next_addr();
             let tcp_sender = TcpSender::new(
                 delay_id,
@@ -60,12 +59,16 @@ pub fn create_topology<'a>(config: &'a Config, tracer: &'a Tracer) -> Result<Sch
             );
             let delay = Delay::new(group_config.delay, link_id);
 
+            // Create the acker
+            let acker = Acker::new(acker_addr, tcp_sender_id);
+
             // Add routes
-            let port = router.add_port(tcp_sender_id);
-            router.add_route(sender_addr, port);
+            let port = router.add_port(acker_id);
+            router.add_route(acker_addr, port);
 
             objs_to_reg.push(Box::new(tcp_sender));
             objs_to_reg.push(Box::new(delay));
+            objs_to_reg.push(Box::new(acker));
         }
     }
 
